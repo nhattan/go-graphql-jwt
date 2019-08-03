a46bd4a45e10ba79fa9083e181f1e863d8c213f3
Tan <4226759+nhattan@users.noreply.github.com>
2019-08-03 23:29:24 +0800

    refactor main

--- main.go
+++ main.go
@@ -3,20 +3,24 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/graphql-go/graphql"
 	"github.com/mitchellh/mapstructure"
-	"github.com/pkg/errors"
 )
 
-type token string
+type tokenKey string
 
 const (
-	t token = "token"
+	myToken tokenKey = "token"
 )
+
+func (tk tokenKey) toString() string {
+	return string(tk)
+}
 
 // User struct
 type User struct {
@@ -33,49 +37,6 @@ type Blog struct {
 	Author    string `json:"author"`
 	Pageviews int32  `json:"pageviews"`
 }
-
-var accountType *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
-	Name: "Account",
-	Fields: graphql.Fields{
-		"id": &graphql.Field{
-			Type: graphql.String,
-		},
-		"username": &graphql.Field{
-			Type: graphql.String,
-		},
-		"password": &graphql.Field{
-			Type: graphql.String,
-		},
-	},
-})
-
-var blogType *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
-	Name: "Blog",
-	Fields: graphql.Fields{
-		"id": &graphql.Field{
-			Type: graphql.String,
-		},
-		"title": &graphql.Field{
-			Type: graphql.String,
-		},
-		"content": &graphql.Field{
-			Type: graphql.String,
-		},
-		"author": &graphql.Field{
-			Type: graphql.String,
-		},
-		"pageviews": &graphql.Field{
-			Type: graphql.Int,
-			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-				_, err := ValidateJWT(params.Context.Value("token").(string))
-				if err != nil {
-					return nil, err
-				}
-				return params.Source.(Blog).Pageviews, nil
-			},
-		},
-	},
-})
 
 var jwtSecret = []byte("thepolyglotdeveloper")
 
@@ -102,6 +63,49 @@ var blogsMock = []Blog{
 	},
 }
 
+var accountType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Account",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.String,
+		},
+		"username": &graphql.Field{
+			Type: graphql.String,
+		},
+		"password": &graphql.Field{
+			Type: graphql.String,
+		},
+	},
+})
+
+var blogType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Blog",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.String,
+		},
+		"title": &graphql.Field{
+			Type: graphql.String,
+		},
+		"content": &graphql.Field{
+			Type: graphql.String,
+		},
+		"author": &graphql.Field{
+			Type: graphql.String,
+		},
+		"pageviews": &graphql.Field{
+			Type: graphql.Int,
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				_, err := ValidateJWT(params.Context.Value(myToken).(string))
+				if err != nil {
+					return nil, err
+				}
+				return params.Source.(Blog).Pageviews, nil
+			},
+		},
+	},
+})
+
 // ValidateJWT validates a JWT token
 func ValidateJWT(t string) (interface{}, error) {
 	if t == "" {
@@ -110,7 +114,7 @@ func ValidateJWT(t string) (interface{}, error) {
 	var err error
 	token, err := jwt.Parse(t, func(token *jwt.Token) (interface{}, error) {
 		if err != nil {
-			return nil, errors.Wrap(err, "Failed to parse the token")
+			return nil, fmt.Errorf("Failed to parse the token: %v", err)
 		}
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("There was an error")
@@ -143,13 +147,14 @@ func CreateTokenEndpoint(response http.ResponseWriter, request *http.Request) {
 }
 
 func main() {
+	fmt.Println("Starting the application at :12345...")
 	rootQuery := graphql.NewObject(graphql.ObjectConfig{
 		Name: "Query",
 		Fields: graphql.Fields{
 			"account": &graphql.Field{
 				Type: accountType,
 				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					account, err := ValidateJWT(params.Context.Value("token").(string))
+					account, err := ValidateJWT(params.Context.Value(myToken).(string))
 					if err != nil {
 						return nil, err
 					}
@@ -174,13 +179,12 @@ func main() {
 		Query: rootQuery,
 	})
 
-	fmt.Println("Starting the application at :12345...")
 	http.HandleFunc("/login", CreateTokenEndpoint)
 	http.HandleFunc("/graphql", func(response http.ResponseWriter, request *http.Request) {
 		result := graphql.Do(graphql.Params{
 			Schema:        schema,
 			RequestString: request.URL.Query().Get("query"),
-			Context:       context.WithValue(context.Background(), t, request.URL.Query().Get("token")),
+			Context:       context.WithValue(context.Background(), myToken, request.URL.Query().Get("token")),
 		})
 		json.NewEncoder(response).Encode(result)
 	})
